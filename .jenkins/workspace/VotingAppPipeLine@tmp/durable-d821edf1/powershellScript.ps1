d18897d3c8d60f7797be92bb60dfcc0bce6911da
pradeepa <pradi.csp@gmail.com>
2021-07-14 09:46:47 +0000

    new files

--- .jenkins/workspace/VotingAppPipeLine@tmp/durable-d821edf1/powershellScript.ps1
+++
@@ -1,2 +0,0 @@
-docker images -a
-exit $LASTEXITCODE
--- .jenkins/workspace/VotingAppPipeLine@tmp/durable-d821edf1/powershellWrapper.ps1
+++
@@ -1,4 +0,0 @@
-[CmdletBinding()]
-param()
-& pwsh -NoProfile -NonInteractive -Command '& {try {& ''/home/labuser/.jenkins/workspace/VotingAppPipeLine@tmp/durable-d821edf1/powershellScript.ps1''} catch {throw}; exit $LASTEXITCODE}'
-exit $LASTEXITCODE
